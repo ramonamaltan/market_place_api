102bf88479191c4d586431e2519ef08774ded217
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-16 11:51:27 +0100

    index action for products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::ProductsController < ApplicationController
   before_action :set_product, only: [:show]
 
+  def index
+    render json: Product.all
+  end
+
   def show
     render json: @product
   end
--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -5,6 +5,13 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     @product = products(:one)
   end
 
+  # test for index
+  test 'should show products' do
+    get api_v1_products_url(), as: :json
+    assert_response :success
+  end
+
+  # test for show
   test 'should show product' do
     get api_v1_product_url(@product), as: :json
     assert_response :success
