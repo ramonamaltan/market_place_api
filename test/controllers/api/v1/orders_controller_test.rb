93ffd831cd308eba62d92e8747778554b8b78d5f
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-18 10:41:16 +0100

    Adds the show action for order

--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -5,6 +5,7 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
     @order = orders(:one)
   end
 
+  # index tests
   test 'should forbid orders for unlogged' do
     get api_v1_orders_url, as: :json
     assert_response :forbidden
@@ -18,5 +19,17 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
 
     json_response = JSON.parse(response.body)
     assert_equal @order.user.orders.count, json_response['data'].count
+  end
+
+  # show tests
+  test 'should show order' do
+    get api_v1_order_url(@order),
+        headers: { Authorization: JsonWebToken.encode(user_id: @order.user_id) },
+        as: :json
+    assert_response :success
+
+    json_response = JSON.parse(response.body)
+    include_product_attr = json_response['included'][0]['attributes']
+    assert_equal @order.products.first.title, include_product_attr['title']
   end
 end
