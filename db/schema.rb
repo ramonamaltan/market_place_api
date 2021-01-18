4ceb9fffff536102d5eaf5a84a7f566a618a72aa
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-18 10:18:08 +0100

    generate order model, test, associations and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_102347) do
+ActiveRecord::Schema.define(version: 2021_01_18_091500) do
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal "total"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.decimal "price"
@@ -30,5 +38,6 @@ ActiveRecord::Schema.define(version: 2021_01_16_102347) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
 end
