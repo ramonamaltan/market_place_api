f58958b7f3b34e45f09e252a243c6be02020de34
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-18 12:05:47 +0100

    Allows the order to be placed along with product quantity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_092031) do
+ActiveRecord::Schema.define(version: 2021_01_18_104530) do
 
   create_table "orders", force: :cascade do |t|
     t.decimal "total"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_092031) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "quantity", default: 0
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
