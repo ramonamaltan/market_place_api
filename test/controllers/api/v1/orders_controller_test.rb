310bc567f19c7785aefcdc7d76302269ff213ca2
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-18 11:03:53 +0100

    Adds the create method for the orders controller

--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -3,6 +3,11 @@ require 'test_helper'
 class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @order = orders(:one)
+
+    @order_params = { order: {
+      product_ids: [products(:one).id, products(:two).id],
+      total: 50
+    } }
   end
 
   # index tests
@@ -31,5 +36,23 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(response.body)
     include_product_attr = json_response['included'][0]['attributes']
     assert_equal @order.products.first.title, include_product_attr['title']
+  end
+
+  # create tests
+  test 'should forbid to create order for unlogged' do
+    assert_no_difference('Order.count') do
+      post api_v1_orders_url, params: @order_params, as: :json
+    end
+    assert_response :forbidden
+  end
+
+  test 'should create order with two products' do
+    assert_difference('Order.count', 1) do
+      post  api_v1_orders_url,
+            params: @order_params,
+            headers: { Authorization: JsonWebToken.encode(user_id: @order.user_id) },
+            as: :json
+    end
+    assert_response :created
   end
 end
